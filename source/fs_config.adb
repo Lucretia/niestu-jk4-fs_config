8d619bfc61d0456dc76c0224e510c9463fd9f9fa
Jerrid Kimball <jkimball@nairobi.home.ada.cx>
2012-10-20 01:47:29 -0500

    Initial commit.

--- source/fs_config.adb
+++ source/fs_config.adb
@@ -4,16 +4,28 @@ with Ada.Text_IO;
 package body FS_Config is
 
    function Get (Name : String) return String is
+   begin
+      return Get (B => Default_Base, Name => Name);
+   end Get;
+
+   procedure Set (Name : String; Value : String) is
+   begin
+      Set (B => Default_Base, Name => Name, Value => Value);
+   end Set;
+
+   function Get (B : Base; Name : String) return String is
       File : Ada.Text_IO.File_Type;
+
+      Full_Path : constant String := B & Name;
 
       Data : String (1 .. 100);
       Last : Natural;
    begin
-      if Ada.Directories.Exists (Ada.Directories.Current_Directory & Name) then
+      if Ada.Directories.Exists (Full_Path) then
          Ada.Text_IO.Open
            (File => File,
             Mode => Ada.Text_IO.In_File,
-            Name => Ada.Directories.Current_Directory & Name);
+            Name => Full_Path);
 
          Ada.Text_IO.Get_Line (File => File, Item => Data, Last => Last);
 
@@ -21,21 +33,24 @@ package body FS_Config is
 
          return Data (1 .. Last);
       else
-         raise Constraint_Error with "file " & Ada.Directories.Current_Directory & Name & " does not exist.";
+         raise Constraint_Error with "file " & Full_Path & " does not exist.";
       end if;
    end Get;
 
    procedure Set
-     (Name  : String;
+     (B     : Base;
+      Name  : String;
       Value : String)
    is
+      Full_Path : constant String := B & Name;
+
       File : Ada.Text_IO.File_Type;
    begin
-      if Ada.Directories.Exists (Ada.Directories.Current_Directory & Name) then
+      if Ada.Directories.Exists (Full_Path) then
          Ada.Text_IO.Open
            (File => File,
             Mode => Ada.Text_IO.Out_File,
-            Name => Ada.Directories.Current_Directory & Name);
+            Name => Full_Path);
 
          Ada.Text_IO.Put_Line (File => File, Item => Value);
 
--- source/fs_config.ads
+++ source/fs_config.ads
@@ -1,9 +1,19 @@
 
 package FS_Config is
 
+   subtype Base is String;
+
+   Default_Base : Base := ".";
+
    function Get (Name : String) return String;
    procedure Set
      (Name  : String;
+      Value : String);
+
+   function Get (B : Base; Name : String) return String;
+   procedure Set
+     (B     : Base;
+      Name  : String;
       Value : String);
 
 end FS_Config;
